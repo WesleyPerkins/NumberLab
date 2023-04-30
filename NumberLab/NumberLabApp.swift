0f18b30020807b04acf0557585582c1c9c390eb0
Wesley Perkins <wesley@wesleyperkins.org>
2023-04-30 15:29:36 +0700

    Initial Commit

---
+++ NumberLab/NumberLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberLabApp.swift
+//  NumberLab
+//
+//  Created by Macintosh on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NumberLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
